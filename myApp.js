37461b87423828ecef9224f1951a339cbe50e39b
batz-gg <87542210+batz-gg@users.noreply.github.com>
2023-04-23 05:48:13 +0000

    m

--- myApp.js
+++ myApp.js
@@ -1,6 +1,47 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+app.use(helmet(
+  {
+    hidePoweredBy: false
+  },
+  {
+    frameguard: { 
+      action: 'DENY' 
+    }
+  },
+  {
+    xssFilter: false
+  },
+  {
+    noSniff: false
+  },
+  {
+    ieNoOpen: false
+  },
+  {
+    ninetyDaysInSeconds: 90*24*60*60,
+    hsts: {
+      maxAge: this.ninetyDaysInSeconds,
+      force: true
+    }
+  },
+  {
+    dnsPrefetchControl: false
+  },
+  {
+    noCache: false
+  },
+  {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'trusted-cdn.com']
+      }
+    }
+  }
+))
 
 
 
@@ -52,7 +93,7 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
